1d6b0e9aaa84fef2609daa728d18dcbbe9730b66
Sahstiva <avitshas@gmail.com>
2021-07-21 19:54:13 +0300

    Домашняя работа к уроку №5

--- index.js
+++ index.js
@@ -1,94 +1,32 @@
+const http = require('http');
 const fs = require('fs');
-const readline = require('readline');
-const yargs = require("yargs");
-const path = require("path");
-const inquirer = require("inquirer");
-const IP = ['89.123.1.41','34.48.240.111'];
-let cwd = __dirname;
-
-let writeStreams = [];
+const path = require('path');
+const url = require('url');
 
 const isFile = fileName => {
     return fs.lstatSync(fileName).isFile();
 }
 
-const options = yargs
-    .usage("Usage: -p <path>")
-    .option("p", { alias: "path", describe: "Path to file", type: "string", demandOption: false })
-    .argv;
-
-const cons = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-if(options.path) {
-    if(isFile(path.join(cwd, options.path)))
-        readfile(path.join(cwd, options.path));
-    else {
-        cwd = cwd + '/' + options.path;
-        getfile(fs.readdirSync(options.path));
-    }
-
-}
-else {
-    console.log('Файл для чтения не задан.');
-    cons.question("Введите имя и путь к файлу или нажмите ENTER для выбора: ", function (input) {
-        if(input === '') {
-            getfile(fs.readdirSync(cwd));
-        } else {
-            readfile(path.join(cwd, input));
-        }
-    });
-}
-function getfile(list) {
-    inquirer
-        .prompt([{
-            name: "fileName",
-            type: "list",
-            message: "Choose file:",
-            choices: list,
-        }])
-        .then((answer) => {
-            if(isFile(path.join(cwd,answer.fileName)))
-                readfile(path.join(cwd, answer.fileName));
-            else {
-                cwd = cwd + '/' + answer.fileName;
-                getfile(fs.readdirSync(answer.fileName));
+http.createServer((request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+    let dirToRead = __dirname;
+    if (request.method && request.method === 'GET') {
+        const queryParams = url.parse(request.url, true).query.selected;
+        if(queryParams) {
+            if(isFile(queryParams)) {
+                response.write(`File: ${queryParams}`);
+                response.end('');
             }
-        });
-}
-function readfile(sourceFile) {
-    console.log('SOURCE = ' + sourceFile);
-    console.time('reading');
-    fs.stat(sourceFile, function(err, stat) {
-        const filesize = stat.size;
-        let bytesRead = 0;
-
-        const readStream = fs.createReadStream(sourceFile, 'utf8');
-        IP.forEach(ip => {
-            writeStreams.push(fs.createWriteStream(`%${ip}%_requests.log`, {flags: 'a', encoding: 'utf8'}));
-        });
-
-        const rl = readline.createInterface({
-            input: readStream,
-            crlfDelay: Infinity
-        });
-
-        rl.on('line', (line) => {
-            bytesRead += line.length;
-            //log(`Progress: ${((bytesRead / filesize) * 100).toFixed(2)}%`);
-            IP.forEach((ip, index) => {
-                if (line.startsWith(ip)) {
-                    writeStreams[index].write(line + '\n');
-                }
-            });
-        });
-
-        rl.on('end', () => console.log('Done'));
-        rl.on('error', err => console.log(err))
-        readStream.on('error', err => console.log(err));
-    });
-}
+            dirToRead = queryParams;
+        }
+    }
+     fs.readdirSync(dirToRead).forEach(item => {
+            response.write(`<p><a href="index.js?selected=${ item }">${ item }</a></p>`);
+     });
 
 
+    response.end('');
+
+
+
+}).listen(3000, 'localhost');
